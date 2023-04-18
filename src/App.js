19f882487e8b3062f23cd2cb1eb1017c37d1e4db
jonio298 <jonio298@gmail.com>
2023-04-18 19:18:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+  const [isCycleActive, setIsCycleActive] = useState(true);
+
+  useEffect(() => {
+    const changeColor = (currentColor) => {
+      switch (currentColor) {
+        case "green":
+          return "yellow";
+        case "yellow":
+          return "red";
+        case "red":
+        default:
+          return "green";
+      }
+    };
+
+    const intervalDuration = (currentColor) => {
+      return currentColor === "yellow" ? 4000 / 3 : 4000;
+    };
+
+    let interval;
+    if (isCycleActive) {
+      interval = setInterval(() => {
+        setColor((prevColor) => changeColor(prevColor));
+      }, intervalDuration(color));
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [color, isCycleActive]);
+
+  const handleButtonClick = () => {
+    setIsCycleActive((prevIsCycleActive) => !prevIsCycleActive);
+  };
+
+  const handleLightClick = (lightColor) => {
+    if (!isCycleActive) {
+      setColor(lightColor);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="traffic-light">
+      <div
+        className={`light red ${
+          color === "red" ? "glow-red" : "gray"
+        }`}
+        onClick={() => handleLightClick("red")}
+      />
+      <div
+        className={`light yellow ${
+          color === "yellow" ? "glow-yellow" : "gray"
+        }`}
+        onClick={() => handleLightClick("yellow")}
+      />
+      <div
+        className={`light green ${
+          color === "green" ? "glow-green" : "gray"
+        }`}
+        onClick={() => handleLightClick("green")}
+      />
+      <button onClick={handleButtonClick}>
+        {isCycleActive ? "Stop" : "Resume"} Cycle
+      </button>
     </div>
   );
 }
